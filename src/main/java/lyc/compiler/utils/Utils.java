5146bd45bfa40b08aaf41fcc6f72caa00692e05a
nicolas.castellino <nicolas.castellino@gurusoluciones.com>
2022-10-23 21:01:09 -0300

    Se agregan generacion de tercetos para las expresiones

--- src/main/java/lyc/compiler/utils/Utils.java
+++ src/main/java/lyc/compiler/utils/Utils.java
@@ -1,6 +1,7 @@
 package lyc.compiler.utils;
 
-import lyc.compiler.model.UnknownCharacterException;
+import lyc.compiler.model.CompilerException;
+import lyc.compiler.model.InvalidExpressionValuesException;
 import lyc.compiler.table.DataType;
 import lyc.compiler.table.SymbolEntry;
 import lyc.compiler.table.SymbolTableManager;
@@ -8,81 +9,18 @@ import lyc.compiler.table.SymbolTableManager;
 import java.util.Stack;
 
 public class Utils {
+    Stack auxExprStack = new Stack<Integer>();
+    int a = (int) auxExprStack.pop();
+    public static boolean checkIdExpresionType(String id, boolean isAssignation) throws CompilerException {
+        SymbolEntry symbol = SymbolTableManager.symbolTable.get(id);
 
-    public static boolean checkExpresionTypes(Stack<DataType> dataTypes, Stack<String> ids){
-
-        DataType d1 = dataTypes.pop();
-        DataType d2 = dataTypes.pop();
-
-        if(d1 == DataType.ID && d2 == DataType.ID){
-            String id1 = ids.pop();
-            String id2 = ids.pop();
-
-            SymbolEntry symbol1 = SymbolTableManager.symbolTable.get(id1);
-            SymbolEntry symbol2 = SymbolTableManager.symbolTable.get(id2);
-
-            if(symbol1.getDataType() == symbol2.getDataType())
-                return true;
-
-            if(symbol1.getDataType() == DataType.INTEGER_TYPE && symbol2.getDataType() == DataType.FLOAT_TYPE)
-                return true;
-
-            if(symbol1.getDataType() == DataType.FLOAT_TYPE && symbol2.getDataType() == DataType.INTEGER_TYPE)
-                return true;
-
-            throw new InvalidExpressionValuesException("Invalid expression value types: " + id1 + " type is not compatible with " + id2 + "type");
-        }
-
-        if(d1 == DataType.ID){
-            String id1 = ids.pop();
-
-            SymbolEntry symbol1 = SymbolTableManager.symbolTable.get(id1);
-
-            if(symbol1.getDataType() == DataType.INTEGER_TYPE && d2 == DataType.INTEGER_CONS)
-                return true;
-
-            if(symbol1.getDataType() == DataType.FLOAT_TYPE && d2 == DataType.FLOAT_CONS)
-                return true;
-
-            if(symbol1.getDataType() == DataType.FLOAT_TYPE && d2 == DataType.INTEGER_CONS)
-                return true;
-
-            if(symbol1.getDataType() == DataType.INTEGER_TYPE && d2 == DataType.FLOAT_CONS)
-                return true;
-
-            throw new InvalidExpressionValuesException("Invalid expression value types: " + id1 + " type is not compatible with " + d2.getName());
-        }
-
-        if(d2 == DataType.ID){
-            String id2 = ids.pop();
-
-            SymbolEntry symbol2 = SymbolTableManager.symbolTable.get(id2);
-
-            if(symbol2.getDataType() == DataType.INTEGER_TYPE && d1 == DataType.INTEGER_CONS)
-                return true;
-
-            if(symbol2.getDataType() == DataType.FLOAT_TYPE && d1 == DataType.FLOAT_CONS)
-                return true;
-
-            if(symbol2.getDataType() == DataType.FLOAT_TYPE && d1 == DataType.INTEGER_CONS)
-                return true;
-
-            if(symbol2.getDataType() == DataType.INTEGER_TYPE && d1 == DataType.FLOAT_CONS)
-                return true;
-
-            throw new InvalidExpressionValuesException("Invalid expression value types: " + id2 + " type is not compatible with " + d1.getName());
-        }
-
-        if(d1 == d2)
+        if(symbol.getDataType() == DataType.FLOAT_TYPE || symbol.getDataType() == DataType.INTEGER_TYPE)
             return true;
 
-        if(d1 == DataType.INTEGER_CONS && d2 == DataType.FLOAT_CONS)
-            return true;
+        if(isAssignation)
+            throw new InvalidExpressionValuesException("Invalid assignation of an expression to String variable");
+        else
+            throw new InvalidExpressionValuesException("Invalid expression value types for id: '" + id + "' type is not compatible for an expression ");
 
-        if(d1 == DataType.FLOAT_CONS && d2 == DataType.INTEGER_CONS)
-            return true;
-
-        throw new InvalidExpressionValuesException("Invalid expression value types: " + d1.getName() + " type is not compatible with " + d2.getName());
     }
-
 }
