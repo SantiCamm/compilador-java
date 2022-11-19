4c7b45cd8d2cbef57d3784e6b349e501a80aa9ed
nicolas.castellino <nicolas.castellino@gurusoluciones.com>
2022-11-19 16:59:45 -0300

    Se agrega logica assembler para condiciones IF

--- src/main/java/lyc/compiler/files/AsmCodeGenerator.java
+++ src/main/java/lyc/compiler/files/AsmCodeGenerator.java
@@ -2,22 +2,25 @@ package lyc.compiler.files;
 
 import lyc.compiler.table.DataType;
 import lyc.compiler.table.SymbolEntry;
-import lyc.compiler.table.SymbolTableManager;
 import lyc.compiler.terceto.Terceto;
 import lyc.compiler.terceto.TercetoManager;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static lyc.compiler.table.SymbolTableManager.symbolTable;
 
 public class AsmCodeGenerator implements FileGenerator {
 
     private HashMap<String, String> constansByValue = new HashMap<>();
+    private Stack<String> conditionLabels = new Stack<>();
+    private int conditionLabelsCont = 1;
+
+    private Stack<String> thenLabels = new Stack<>();
+    private int thenLabelsCont = 1;
+
+    private HashMap<String, String> jumpLabels = new HashMap<String, String>();
 
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
@@ -93,9 +96,22 @@ public class AsmCodeGenerator implements FileGenerator {
                 case "-":
                     writeExpression(fileWriter, terceto, "-");
                     break;
+                case "CMP":
+                    writeDecition(fileWriter, terceto);
+                    break;
+                case "THEN":
+                    writeThenLabel(fileWriter);
+                    break;
+                case "BI":
+                    writeInconditionalJump(fileWriter, terceto);
+                    break;
+                case "endif":
+                    writeLabel(fileWriter);
+                    break;
             }
         }
     }
+
 
     private void writeAsignation(FileWriter fileWriter, Terceto terceto) throws IOException {
 
@@ -117,11 +133,11 @@ public class AsmCodeGenerator implements FileGenerator {
             aux = constansByValue.get(third.replace("\"", ""));
 
         if(aux != "+" && aux != "-" && aux != "*" && aux != "/" && aux != null){
-            fileWriter.write("fld " + aux + "\n");
+            fileWriter.write("FLD " + aux + "\n");
         }
 
-        fileWriter.write("fstp " + terceto.getSecond() + "\n");
-        fileWriter.write("ffree\n\n");
+        fileWriter.write("FSTP " + terceto.getSecond() + "\n");
+        fileWriter.write("FFREE\n\n");
     }
 
     private void writeExpression(FileWriter fileWriter, Terceto terceto, String operator) throws IOException {
@@ -129,10 +145,10 @@ public class AsmCodeGenerator implements FileGenerator {
         String asmOperator = "";
 
         switch (operator){
-            case "+" -> asmOperator = "fadd";
-            case "-" -> asmOperator = "fsub";
-            case "*" -> asmOperator = "fmul";
-            case "/" -> asmOperator = "fdiv";
+            case "+" -> asmOperator = "FADD";
+            case "-" -> asmOperator = "FSUB";
+            case "*" -> asmOperator = "FMUL";
+            case "/" -> asmOperator = "FDIV";
         }
 
         int pos1 = Integer.valueOf(terceto.getSecond().replace("[", "").replace("]", "")) - 1;
@@ -146,8 +162,75 @@ public class AsmCodeGenerator implements FileGenerator {
         if(aux2 == null)
             aux2 = TercetoManager.tercetoList.get(pos1).getFirst();
 
-        fileWriter.write("fld " + aux1 + "\n");
+        fileWriter.write("FLD " + aux1 + "\n");
         fileWriter.write(asmOperator + " " + aux2 + "\n");
+    }
+
+    public void writeDecition(FileWriter fileWriter, Terceto terceto) throws IOException {
+        fileWriter.write("FLD " + terceto.getSecond() + "\n");
+        fileWriter.write("FCOMP " + terceto.getThird() + "\n");
+
+        fileWriter.write("FSTSW ax\n");
+        fileWriter.write("SAHF \n");
+
+
+        Terceto jumpTerceto = TercetoManager.tercetoList.get(terceto.getNumber());
+        String jumpType = getJump(jumpTerceto.getFirst());
+
+        Terceto nextToJump = TercetoManager.tercetoList.get(terceto.getNumber() + 1);
+
+
+
+        if(nextToJump.getFirst() == "OR" || nextToJump.getFirst() == "THEN"){
+            String thenLabel = "then_" + thenLabelsCont;
+            thenLabels.push(thenLabel);
+            thenLabelsCont++;
+        }
+
+        String  conditionLabel = jumpLabels.get(jumpTerceto.getSecond());
+            if(conditionLabel == null){
+                conditionLabel = "else_" + conditionLabelsCont;
+                jumpLabels.put(jumpTerceto.getSecond(),conditionLabel);
+                conditionLabelsCont++;
+                conditionLabels.push(conditionLabel);
+            }
+
+
+        fileWriter.write(jumpType + " " + conditionLabel + "\n");
+
+    }
+
+    private void writeInconditionalJump(FileWriter fileWriter, Terceto terceto) throws IOException {
+
+        String conditionLabel = "endif_" + conditionLabelsCont;
+
+        fileWriter.write("JMP " + conditionLabel + "\n\n");
+        fileWriter.write(conditionLabels.pop() + ":\n");
+
+        conditionLabelsCont++;
+        conditionLabels.push(conditionLabel);
+    }
+
+    private void writeLabel(FileWriter fileWriter) throws IOException {
+        String label = conditionLabels.pop();
+        fileWriter.write("\n"+ label + ":\n");
+    }
+
+    private void writeThenLabel(FileWriter fileWriter) throws IOException {
+        String label = thenLabels.pop();
+        fileWriter.write("\n"+ label + ":\n");
+    }
+    private String getJump(String comparator) {
+        String jump = null;
+        switch (comparator){
+            case "BLE" -> jump = "JLE";
+            case "BGE" -> jump = "JGE";
+            case "BLT" -> jump = "JB";
+            case "BGT" -> jump = "JA";
+            case "BNE" -> jump = "JNE";
+            case "BQE" -> jump = "JE";
+        }
+        return jump;
     }
 
     private String tabCalculator(String value, int columnWidth){
